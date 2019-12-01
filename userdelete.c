d1bf2144f61a7be87a822b1eb73c1b0c62f331c2
pmi4202 <pmi4202@naver.com>
2019-12-01 16:36:56 +0900

    user name reading

--- userdelete.c
+++ userdelete.c
@@ -1,7 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <utmp.h>
+
+void show_info();
 
 int main(){
+	struct utmp current_record;
+	int utmpfd;
+	int reclen=sizeof(current_record);
 
+	if((utmpfd=open(UTMP_FILE, O_RDONLY)) ==-1){
+		perror(UTMP_FILE);
+		exit(1);
+	}
+	while(read(utmpfd, &current_record, reclen)==reclen){
+		show_info(&current_record);
+	}
+
+	close(utmpfd);
 	return 0;
+}
+
+void show_info(struct utmp *utbufp){
+	if(utbufp->ut_type !=USER_PROCESS)
+		return;
+	printf("%s\n", utbufp->ut_name);
 }
