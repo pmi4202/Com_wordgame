e4187be64607c2a73dc0481b8ab4f721ee453b9b
KHyoseon <apso123@naver.com>
2019-12-14 12:57:31 +0900

    Add files via upload

--- gamePage.c
+++ gamePage.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main(){
 	// game_search 함수에 추가
 	int correct, user_score = 0;
-	int i, j;
+	int i=0, j;
 	char nowquiz[15][BUFSIZ];
+	char co[5][BUFSIZ], wr[5][BUFSIZE];
 
 	// 문장 읽어올 때 마다 실행
-	user_score += quiz_release(nowquiz[quiznum*3], user_score);
+	correct = quiz_release(nowquiz[quiznum*3]);
+	// 맞았을 경우
+	if(correct){
+		user_score++;
+		printf("	Correct!!\n\n");
+	}
+	else{
+		printf("	Wrong!! The answer is %s\n\n", nowquiz[quiznum*3+2]);
+	}
 
 	// 반복 끝나면
 	printf("The test is finished. Your Score is ... %d", user_score);
 }
 
-void quiz_release(quiz nowquiz, int user_score){
-	int user_answ;
+void quiz_release(char *nowquiz){
+	char user_answ;
 
-	printf("%s\n", nowquiz.question);
-	printf("%s\n => ", nowquiz.example);
-	scanf("%d", user_answ);
+	printf("%s\n", nowquiz);
+	printf("%s\n => ", nowquiz+1);
+	scanf("%c", user_answ);
 
 	/* 유저가 정답을 맞추면 1 반환, 틀리면 0 반환 */
-	if(user_answ == nowquiz.answer){
-		user_score++;
+	if(strcmp(user_answ, nowquiz+2)){
 		return 1;
 	}
 	else
