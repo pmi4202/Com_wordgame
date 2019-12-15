47622928700266daee13b92f72b2f3c15b31c86a
KHyoseon <apso123@naver.com>
2019-12-15 14:50:17 +0900

    Add files via upload

--- gamePage.c
+++ gamePage.c
@@ -1,39 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
+#include <termios.h>
+#include	<unistd.h> 
+
+int correct = 0;
+int skip = 0;
+
+void quiz_release(char *nowquiz);
+void time_out();
 
 int main(){
 	// game_search 함수에 추가
-	int correct, user_score = 0;
-	int i=0, j;
-	char nowquiz[15][BUFSIZ];
-	char co[5][BUFSIZ], wr[5][BUFSIZE];
-
+	int user_score = 0;
+	char nowquiz[15][BUFSIZ] = {"question...", "number...", "2"};
+	int quiznum = 0;
 	// 문장 읽어올 때 마다 실행
-	correct = quiz_release(nowquiz[quiznum*3]);
-	// 맞았을 경우
-	if(correct){
-		user_score++;
-		printf("	Correct!!\n\n");
-	}
-	else{
-		printf("	Wrong!! The answer is %s\n\n", nowquiz[quiznum*3+2]);
-	}
+	quiz_release(nowquiz[quiznum*3]);
+	user_score += correct;
 
 	// 반복 끝나면
-	printf("The test is finished. Your Score is ... %d", user_score);
+	printf("The test is finished. Your Score is ... %d\n", user_score);
 }
 
-void quiz_release(char *nowquiz){
+void quiz_release(char *nowquiz)
+{
 	char user_answ;
+	char c;
 
 	printf("%s\n", nowquiz);
-	printf("%s\n => ", nowquiz+1);
-	scanf("%c", user_answ);
+	printf("%s\n		=> ", nowquiz+(1*BUFSIZ));
+	
+	signal(SIGALRM, time_out);
+	alarm(10);
 
-	/* 유저가 정답을 맞추면 1 반환, 틀리면 0 반환 */
-	if(strcmp(user_answ, nowquiz+2)){
-		return 1;
+	c = getc(stdin);
+
+	if(!skip)
+	{
+		if( !strcmp(&c, nowquiz+(2*BUFSIZ)) )
+		{
+			correct = 1;
+			alarm(0);
+			printf("Correct!\n");
+		}
+		else
+		{
+			correct = 0;
+			alarm(0);
+			printf("Wrong! The anwser is %s!\n", nowquiz+(2*BUFSIZ));
+		}
 	}
-	else
-		return 0;	
+	printf("Go to next question in 3 sec ...\n");
+	sleep(3);
+	return;
+}
+
+
+void time_out()
+{
+	printf("\n\tTime out!\npress any keyboards\n");
+	skip++;
+	correct = 0;
+	return;
 }
